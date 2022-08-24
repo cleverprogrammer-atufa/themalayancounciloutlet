781713fb71caf651bf28d3b541573bf1f5c168eb
Atufa <atufafaheem@gmail.com>
2022-08-24 12:23:47 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function validate() {
+	var name = document.stud_form.email;
+    var subject = document.stud_form.subject;
+    var email = document.stud_form.email;
+    var message = document.stud_form.message;
+	if (name.value.length <=0) {
+		error("Name is required");
+		return false;
+	}
+}
